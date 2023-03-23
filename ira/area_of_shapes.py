af5c424b8a944e93f98aea19ad6a9cb8409b612e
IraMuomah <muomaho@gmail.com>
2023-03-23 07:45:51 -0700

    I stated the contribution i made,and added my prjcts

---
+++ ira/area_of_shapes.py
@@ -0,0 +1,11 @@
+# # area of a circle
+# r = float(input("input the radius: ") )  
+# area1 = 3.14*r*r
+# print("the area is: ",area1)
+
+
+# area of a rectangle
+l = int(input("input the lenth: ") )  
+w = int(input("input the width: ") )  
+area2 = l*w
+print("the area is: ",area2)
---
+++ ira/dollar_to_naira.py
@@ -0,0 +1,4 @@
+d = int(input("type dollar amount to convert: ") )      
+rate = int(input("input exchange rate: ") )  
+n = d*rate
+print("N",n)
