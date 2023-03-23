1bfa12eb9e793fec4dd13d9d80224b125384b20d
Alade joseph <aladejoseph656@gmail.com>
2023-03-23 14:54:59 +0100

    stated

---
+++ spec-birthyear.py
@@ -0,0 +1,4 @@
+year=int(input("input your birth year: "))
+currentYear = int(input("input the current year: "))
+age=currentYear-year
+print("you will be",age, "this year")
---
+++ spec-circleArea.py
@@ -0,0 +1,4 @@
+Pi= float(22/7)
+radius = int(input("input radius: "))
+area = Pi * radius*radius
+print(area)
